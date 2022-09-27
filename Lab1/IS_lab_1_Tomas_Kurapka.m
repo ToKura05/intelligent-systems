59b4f54f72855a2af488fd590d66d632b6544696
Tomas Kurapka <t.kurapka@gmail.com>
2022-09-27 20:54:54 +0300

    master: Added comments and initial sum of errors calculation without updating parameters

--- Lab1/IS_lab_1_Tomas_Kurapka.m
+++ Lab1/IS_lab_1_Tomas_Kurapka.m
@@ -1,5 +1,5 @@
 %% Perceptrono apmokymo algoritmas
-
+clear
 % Sugeneruojamos pradines w1, w2 ir b reiksmes
 w1 = randn(1);
 w2 = randn(1);
@@ -14,11 +14,30 @@ x2 = [0.83449, 0.84049, 0.77104, 0.62068,  0.79092, 0.83535, 0.80889];
 d  = [1, 1, -1, -1, -1, 1, 1];
 
 % Priskiriama pradine reiksme >0 klaidu sumai, kad ciklas pradėtų suktis
-e_sum = 1;
+e_sum = 0;
+
+
+% iteruojame per visus pavyzdzius, neatnaujinant parametru
+for indx = 1 : length(d)
+    % skaiciuojama pasvertoji suma
+    v = x1(indx)*w1 + x2(indx)*w2 + b;
+    % skaiciuojamas perceptrono isejimas
+    if v > 0
+        y = 1;
+    else
+        y = -1;
+    end
+    % skaiciuojama klaida
+    e(indx) = d(indx) - y;
+    % skaiciuojama klaidu suma
+    e_sum = e_sum + e(indx);
+end
+
+% Kadangi is pradziu parinkus atsitiktinius parametrus praktiskai niekada
+% negausime klaidu sumos lygios 0, reikia perceptrona apmokyti.
+% Mokymas vyksta, kol e_sum nelygus 0
 
 while e_sum ~= 0
-
-    % iteruojame per visus pavyzdzius
     for indx = 1 : length(d)
         % skaiciuojama pasvertoji suma
         v = x1(indx)*w1 + x2(indx)*w2 + b;
@@ -37,13 +56,15 @@ while e_sum ~= 0
             b  = b  + n*e(indx);
         end
     end
-% po visu iteraciju skaiciuojama visu pavyzdziu klaidu absoliuciu verciu
-% suma. Jai esant 0, laikome, kad perceptronas "apmokytas"
-e_sum = sumabs(e);
-
+    % po visu iteraciju skaiciuojama visu pavyzdziu klaidu absoliuciu verciu
+    % suma. Jai esant 0, laikome, kad perceptronas "apmokytas"
+    e_sum = sumabs(e);
+    % jeigu ne, mokymas tesiamas is naujo iteruojant per visus pozymius
 end
 
-% duomenys atvaizduojami dvejose asyse
+% duomenys atvaizduojami dvejose asyse (o - atitinka obuolius, k - atitinka
+% kriauses)
+
 x1o = [0.55223, 0.16975, 0.14115, 0.49187];
 x2o = [0.83449, 0.84049, 0.83535, 0.80889];
 
